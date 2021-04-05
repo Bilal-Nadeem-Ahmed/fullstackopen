c5ff4e2ebb94a011ed38128b69e0cc6fe7f3768d
Bilal Ahmed <bilal91_@hotmail.com>
2021-04-05 17:01:30 +0100

    finished 1.3

--- part1/courseInfo/my-app/src/components/Content/index.js
+++ part1/courseInfo/my-app/src/components/Content/index.js
@@ -1,19 +1,20 @@
 import Part from "./components/Part";
 
 const Content = (props) => {
+    console.log(props)
     return ( 
         <>
         <Part
-        part={props.part1}
-        exercise={props.exercises1}
+        part={props.part1.name}
+        exercise={props.part1.exercises}
+        />
+        <Part
+        part={props.part2.name}
+        exercise={props.part2.exercises}
         />
          <Part
-        part={props.part2}
-        exercise={props.exercises2}
-        />
-         <Part
-        part={props.part3}
-        exercise={props.exercises3}
+        part={props.part3.name}
+        exercise={props.part3.exercises}
         />
         </>
      );
