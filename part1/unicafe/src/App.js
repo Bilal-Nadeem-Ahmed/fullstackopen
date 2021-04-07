5adc3452e65cbb51eafa6f97d7a9b4ba938ec687
Bilal Ahmed <bilal91_@hotmail.com>
2021-04-07 13:43:46 +0100

    done 1.8

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,15 +1,13 @@
 import './App.css';
 import {useState} from 'react'
 import Button from './components/button';
+import Statistics from './components/statistics';
 
 function App() {
 
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const total=good +neutral+bad
-  const average = (good - bad )/ total
-  const positive = (good /total) * 100 
   const handleClick=(x,y)=>y(x+1)
   return (
     <div className="App">
@@ -17,13 +15,8 @@ function App() {
       <Button title='good' handleClick={()=>handleClick(good,setGood)}/>
       <Button title='neutral' handleClick={()=>handleClick(neutral,setNeutral)}/>
       <Button title='bad' handleClick={()=>handleClick(bad,setBad)}/>
-      <h2>Statistics</h2>
-      <p> good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>total {total}</p>
-      <p>average {average?average :0}</p>
-      <p>positive {positive? positive: 0} %</p>
+     
+      <Statistics good={good} neutral={neutral} bad ={bad}/>
     </div>
   );
 }
---
+++ part1/unicafe/src/components/statistics/index.js
@@ -0,0 +1,16 @@
+const Statistics = ({good,neutral,bad}) => {
+    const total=good +neutral+bad
+    const average = (good - bad )/ total
+    const positive = (good /total) * 100 
+    return (<>
+     <h2>Statistics</h2>
+      <p> good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p>total {total}</p>
+      <p>average {average?average :0}</p>
+      <p>positive {positive? positive: 0} %</p>
+    </>);
+}
+ 
+export default Statistics;
