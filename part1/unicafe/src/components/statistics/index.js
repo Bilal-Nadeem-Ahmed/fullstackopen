36b2075e03ddb7e59e91913c19f0d65f5cb2cb79
Bilal Ahmed <bilal91_@hotmail.com>
2021-04-07 14:20:06 +0100

    done 1.10

--- part1/unicafe/src/components/statistics/index.js
+++ part1/unicafe/src/components/statistics/index.js
@@ -1,19 +1,22 @@
+import Statistic from "../statistic"
+
 const Statistics = ({good,neutral,bad}) => {
     const total=good +neutral+bad
     const average = (good - bad )/ total
     const positive = (good /total) * 100 
+    
     if( total <1) {
         return <p>No Feedback given</p>
     }
 
     return (  <>
      <h2>Statistics</h2>
-      <p> good {good}</p>
-      <p>neutral {neutral}</p>
-      <p>bad {bad}</p>
-      <p>total {total}</p>
-      <p>average {average?average :0}</p>
-      <p>positive {positive? positive: 0} %</p>
+     <Statistic text ='good' value ={good}/>
+     <Statistic text ='neutral' value ={neutral}/>
+     <Statistic text ='bad' value ={bad}/>
+     <Statistic text ='total' value ={total}/>
+     <Statistic text ='average' value ={average}/>
+     <Statistic text ='positive' value ={`${positive} %`}/>
     </>
     )
 }
