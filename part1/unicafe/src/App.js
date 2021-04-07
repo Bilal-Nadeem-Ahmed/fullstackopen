1d73e22e693bfc9f1be85ed546f363a063ff4945
Bilal Ahmed <bilal91_@hotmail.com>
2021-04-07 12:36:31 +0100

    done 1.6

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
+import Button from './components/button';
 
 function App() {
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const handleClick=(x,y)=>y(x+1)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Give Feedback</h1>
+      <Button title='good' handleClick={()=>handleClick(good,setGood)}/>
+      <Button title='neutral' handleClick={()=>handleClick(neutral,setNeutral)}/>
+      <Button title='bad' handleClick={()=>handleClick(bad,setBad)}/>
+      <h2>Statistics</h2>
+      <p> good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
     </div>
   );
 }
