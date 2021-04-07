c035582eea6ea0ef9b843befe055f066da4711b6
Bilal Ahmed <bilal91_@hotmail.com>
2021-04-07 13:46:51 +0100

    done 1.9

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -8,15 +8,16 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
-  const handleClick=(x,y)=>y(x+1)
+  const [feedback,setFeedback]=useState(false)
+  const handleClick=(x,y)=>(y(x+1),setFeedback(true))
   return (
     <div className="App">
       <h1>Give Feedback</h1>
       <Button title='good' handleClick={()=>handleClick(good,setGood)}/>
       <Button title='neutral' handleClick={()=>handleClick(neutral,setNeutral)}/>
       <Button title='bad' handleClick={()=>handleClick(bad,setBad)}/>
-     
-      <Statistics good={good} neutral={neutral} bad ={bad}/>
+     {feedback? <Statistics good={good} neutral={neutral} bad ={bad}/> : null}
+      
     </div>
   );
 }
