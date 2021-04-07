20295edee9bbdd13015a196a6473123880a8cd9f
Bilal Ahmed <bilal91_@hotmail.com>
2021-04-07 13:35:38 +0100

    done 1.7

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -7,6 +7,9 @@ function App() {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const total=good +neutral+bad
+  const average = (good - bad )/ total
+  const positive = (good /total) * 100 
   const handleClick=(x,y)=>y(x+1)
   return (
     <div className="App">
@@ -18,6 +21,9 @@ function App() {
       <p> good {good}</p>
       <p>neutral {neutral}</p>
       <p>bad {bad}</p>
+      <p>total {total}</p>
+      <p>average {average?average :0}</p>
+      <p>positive {positive? positive: 0} %</p>
     </div>
   );
 }
