f5bb9d8e7e1ba093f630e392e67dd0c3cea39c18
Bilal Ahmed <bilal91_@hotmail.com>
2021-04-20 22:55:43 +0100

    done 2.11

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,15 +1,22 @@
-import  { useState } from 'react';
+import  { useState,useEffect } from 'react';
 import Filter from './Components/filter';
 import PersonForm from './Components/personForm';
 import Persons from './Components/persons';
-
+import axios from 'axios'
 function App() {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '07455483344' },
-    { name: 'Ada Lovelace', number: '39-44-5323523' },
-    { name: 'Dan Abramov', number: '12-43-234345' },
-    { name: 'Mary Poppendieck', number: '39-23-6423122' }
-  ]) 
+  const [ persons, setPersons ] = useState([]) 
+  useEffect(()=>{
+    axios.get('http://localhost:3001/persons')
+    .then(res=>setPersons(res.data))
+
+  },[])
+  
+  // const [ persons, setPersons ] = useState([
+  //   { name: 'Arto Hellas', number: '07455483344' },
+  //   { name: 'Ada Lovelace', number: '39-44-5323523' },
+  //   { name: 'Dan Abramov', number: '12-43-234345' },
+  //   { name: 'Mary Poppendieck', number: '39-23-6423122' }
+  // ]) 
   const [ newName, setNewName ] = useState('')
   const [newNumber,setNewNumber] = useState('')
   const [filterCriteria,setFilterCriteria] = useState('')
