58c1c07836c080918ea16e702516eae871da3c42
Bilal Ahmed <bilal91_@hotmail.com>
2021-05-28 16:47:19 +0100

    done 4.4

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -3,28 +3,28 @@ const Blog = require('../models/blog')
 
 blogRouter.get('/', (request, response) => {
     Blog
-      .find({})
-      .then(blogs => {
-        response.json(blogs)
-      })
+        .find({})
+        .then(blogs => {
+            response.json(blogs)
+        })
   
-  })
+})
   
- blogRouter.post('/', (request, response) => {
+blogRouter.post('/', (request, response) => {
     const blog = new Blog(request.body)
   
-  // const body = request.body
-  // const blog = new Blog({
-  //     title: body.title,
-  //   author: body.author,
-  //   url: body.url,
-  //   likes: body.likes
-  // })
+    // const body = request.body
+    // const blog = new Blog({
+    //     title: body.title,
+    //   author: body.author,
+    //   url: body.url,
+    //   likes: body.likes
+    // })
     blog
-      .save()
-      .then(result => {
-        response.status(201).json(result)
-      })
-  })
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+})
 
-  module.exports=blogRouter
+module.exports=blogRouter
--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -1,12 +1,32 @@
+// eslint-disable-next-line no-unused-vars
 const dummy =(blogs)=>{
     return 1
 }
 
 const totalLikes = (list)=>{
-let likes = 0
-list.forEach(blogItem=> likes+= blogItem.likes)   
-return likes 
+    let likes = 0
+    list.forEach(blogItem=> likes+= blogItem.likes)   
+    return likes 
+}
+
+const favouriteBlog =(list)=>{
+
+    if(list.length>0){
+        const likesArray = list.map((listItem)=>listItem.likes)
+        const indexOfHighestLikes = likesArray.indexOf(Math.max(...likesArray))
+    
+        return {
+            title: list[indexOfHighestLikes].title,
+            author: list[indexOfHighestLikes].author,
+            likes: list[indexOfHighestLikes].likes
+        }
+    } else{
+        return 'you have failed to pass through the bloglist correctly'
+    }
+
+   
+
 }
 
 
-module.exports={dummy,totalLikes}
+module.exports={dummy,totalLikes,favouriteBlog}
