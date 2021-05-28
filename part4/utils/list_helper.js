a2e91d0b3c89c71648832f4ad25c6221ba3f4f96
Bilal Ahmed <bilal91_@hotmail.com>
2021-05-28 15:31:41 +0100

    done 4.4

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,5 +2,11 @@ const dummy =(blogs)=>{
     return 1
 }
 
+const totalLikes = (list)=>{
+let likes = 0
+list.forEach(blogItem=> likes+= blogItem.likes)   
+return likes 
+}
 
-module.exports={dummy}
+
+module.exports={dummy,totalLikes}
