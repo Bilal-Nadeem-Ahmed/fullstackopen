61a92d6a1d5d0aff5c6f5ab36c70da67a7419e42
Bilal Ahmed <bilal91_@hotmail.com>
2021-07-06 20:17:59 +0100

    done 4.10

--- part4/controllers/blogs.js
+++ part4/controllers/blogs.js
@@ -2,29 +2,47 @@ const blogRouter = require('express').Router()
 const Blog = require('../models/blog')
 
 blogRouter.get('/', (request, response) => {
-    Blog
-        .find({})
-        .then(blogs => {
-            response.json(blogs)
-        })
-  
+  Blog
+    .find({})
+    .then(blogs => {
+      response.json(blogs)
+    })
+
 })
-  
+
+blogRouter.get('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+
+  if (blog) {
+    response.json(blog)
+  } else {
+    response.status(404).end()
+  }
+
+})
+
 blogRouter.post('/', (request, response) => {
-    const blog = new Blog(request.body)
-  
-    // const body = request.body
-    // const blog = new Blog({
-    //     title: body.title,
-    //   author: body.author,
-    //   url: body.url,
-    //   likes: body.likes
-    // })
-    blog
-        .save()
-        .then(result => {
-            response.status(201).json(result)
-        })
+  const blog = new Blog(request.body)
+
+  // const body = request.body
+  // const blog = new Blog({
+  //     title: body.title,
+  //   author: body.author,
+  //   url: body.url,
+  //   likes: body.likes
+  // })
+  blog
+    .save()
+    .then(result => {
+      response.status(201).json(result)
+    })
+})
+
+blogRouter.delete('/:id', async (request, response) => {
+
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+
 })
 
 module.exports=blogRouter
--- part4/tests/blogs_api.test.js
+++ part4/tests/blogs_api.test.js
@@ -4,12 +4,20 @@ const app= require('../app')
 
 const api = supertest(app)
 // had an issue where test would not pass. This was due to the / not being present
-test('notes are returned as json', async() => {
+test('blogs are returned as json', async() => {
   console.log('entered first test')
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
+})
+
+test('check that the id property is correctly defined', async() => {
+  const blogs= await api.get('/api/blogs/')
+  const firstBlogsId = blogs.body[0].id
+
+
+  expect(firstBlogsId).toBeDefined()
 })
 
 afterAll(() => {
