d1eb71831b9d0b07b54c42146351d7b41c1dd346
Bilal Ahmed <bilal91_@hotmail.com>
2021-07-06 22:32:42 +0100

    done 4.10 now, previous commit was for .9

--- part4/app.js
+++ part4/app.js
@@ -11,16 +11,16 @@ const mongoose = require('mongoose')
 logger.info('connected to', config.MONGODB_URI)
 
 mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
-    .then(()=>{
-        logger.info('connected to MongoDB')
-    })
-    .catch((error)=>{
-        logger.error('connected to MongoDB:', error.message )
-    })
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('connected to MongoDB:', error.message )
+  })
 
 
 app.use(cors())
-// app.use(express.static('build'))
+app.use(express.static('build'))
 app.use(express.json())
 
 // app.use(middleware.requestlogger)
--- part4/tests/blogs_api.test.js
+++ part4/tests/blogs_api.test.js
@@ -1,8 +1,21 @@
 const mongoose=require('mongoose')
 const supertest = require('supertest')
+const helper= require('./tests_helper')
 const app= require('../app')
 
 const api = supertest(app)
+
+const Blog = require('../models/blog')
+
+beforeEach(async() => {
+  await Blog.deleteMany({})
+
+  for(let blog of helper.initialBlogs){
+    let blogObject= new Blog(blog)
+    await blogObject.save()
+  }
+
+})
 // had an issue where test would not pass. This was due to the / not being present
 test('blogs are returned as json', async() => {
   console.log('entered first test')
@@ -18,6 +31,29 @@ test('check that the id property is correctly defined', async() => {
 
 
   expect(firstBlogsId).toBeDefined()
+})
+
+test('a blog can be added', async () => {
+  const newBlog = {
+    'title': 'a simple guide',
+    'author': 'Bilal Ahmed',
+    'url': 'https://asimpleguide.dev/',
+    'likes': 5
+  }
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const blogsAtEnd = await helper.blogsInDb()
+  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length +1)
+
+  const contents = blogsAtEnd.map(r => r.title)
+
+
+  expect(contents).toContain('a simple guide')
 })
 
 afterAll(() => {
