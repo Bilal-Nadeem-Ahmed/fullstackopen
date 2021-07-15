30fb6991051887b7bb4df02962716f75f288cca1
Bilal Ahmed <bilal91_@hotmail.com>
2021-07-15 15:06:48 +0100

    done part 17, 18 & 19

--- part4/app.js
+++ part4/app.js
@@ -5,6 +5,7 @@ const app = express()
 const cors = require('cors')
 const blogRouter=require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 // const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
@@ -23,6 +24,7 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use('/api/login', loginRouter)
 
 // app.use(middleware.requestlogger)
 
