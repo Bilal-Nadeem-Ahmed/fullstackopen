c8edc15b6908bc5d190893ed7385ef073f27df38
Bilal Ahmed <bilal91_@hotmail.com>
2021-07-15 11:23:55 +0100

    done 4.15

---
+++ part4/controllers/users.js
@@ -0,0 +1,27 @@
+const bcrypt = require('bcrypt')
+const usersRouter = require('express').Router()
+const User = require('../models/user')
+
+usersRouter.post('/', async (request, response) => {
+  const body = request.body
+
+  const saltRounds = 10
+  const passwordHash = await bcrypt.hash(body.password, saltRounds)
+
+  const user = new User({
+    username: body.username,
+    name: body.name,
+    passwordHash,
+  })
+
+  const savedUser = await user.save()
+
+  response.json(savedUser)
+})
+usersRouter.get('/', async (request, response) => {
+  // populte is getting the link between the notes and the user and bringing that data here
+  const users = await User.find({}).populate('blog',{ title: 1,author:1,url:1,likes:1 })
+  response.json(users)
+})
+
+module.exports = usersRouter
