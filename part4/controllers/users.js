02f54ef5f1ef666649dc625a41c9bb777fcd8c69
Bilal Ahmed <bilal91_@hotmail.com>
2021-07-15 14:52:07 +0100

    done 4.16

--- part4/controllers/users.js
+++ part4/controllers/users.js
@@ -5,6 +5,12 @@ const User = require('../models/user')
 usersRouter.post('/', async (request, response) => {
   const body = request.body
 
+  if (body.username.length<3 ||body.password.length<3) {
+    return response.status(401).json({
+      error: 'the username or password is too short, please ensure it is at least three charactesr long'
+    })
+  }
+
   const saltRounds = 10
   const passwordHash = await bcrypt.hash(body.password, saltRounds)
 
